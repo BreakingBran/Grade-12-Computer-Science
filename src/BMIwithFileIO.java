fc3c812c1fb4a0d74c751f2845e1d7df4042ab42
073309882 <073309882@CENTRE-CTX-RD23.educ.dpcdsb.org>
2017-02-16 09:22:47 -0500

    God I hope this works

---
+++ src/BMIwithFileIO.java
@@ -0,0 +1,60 @@
+/**
+ * BMIwithFileIO
+ * @author: Lance Pereira
+ * @course: ICS4U1
+ * @date: Feb 16, 2017
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class BMIwithFileIO
+{
+  //Read in weight and height
+  //Calculate BMI 
+  //Determine weight level
+  
+  public static void main(String args[]) throws IOException
+  {
+    // Read Weight and Height 
+    
+    //initializes the input function
+	  
+    //Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(new FileReader("BMIinput.txt"));
+    
+    PrintWriter pw = new PrintWriter(new FileWriter("BMIoutput.txt"));
+    // Using doubles b/c it is more accurate
+    //System.out.print("Enter Weight:");
+    double weight = sc.nextFloat();
+    
+    //System.out.print("Enter Height:");
+    double height = sc.nextFloat();
+    
+    // Calculate BMI
+    
+    double BMI = (weight/Math.pow(height,2));
+      
+     //Determine Weight level
+     
+     String health = "Error";
+     if(BMI > 25)
+    {
+    health = "Overweight";
+    }
+    else if ((18.5 <= BMI) && (BMI <= 25.0))
+    {
+    health = "Normal Weight";
+    }
+    else if (BMI < 18.5)
+    {
+    health = "Underweight";
+    }
+    
+   //System.out.println(health);
+   pw.print(health);
+   pw.flush();
+   pw.close();
+     
+  }
+}
