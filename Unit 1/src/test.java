06a1399dc60212a6e58fe34e91ba317f5c02c31e
BreakingBran <lancepereira99@gmail.com>
2017-02-28 21:00:41 -0500

    Finished all flowcharts

---
+++ Unit 1/src/test.java
@@ -0,0 +1,24 @@
+/**
+ * GPS
+ * @author: Lance Pereira
+ * @course: ICS4U1
+ * @date: Feb 25, 2017
+ */
+
+import java.util.*;
+import java.io.*;
+
+
+public class test
+{
+  public static void main(String args[])
+  {
+    //parse string and look at each indivizual character
+    //each character corosponds to row and colunm value
+    // determine diffirence between row and colunm and add to toal turns
+    int a = 2;
+    int b;
+    b = (int)  a*1.5;
+     System.out.println(a*1.5);
+  }
+}
