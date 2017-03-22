6ee30576d5a90cbd5a164eb364eca13ccab555a3
Pereira <073309882@educ.dpcdsb.org>
2017-03-22 09:16:50 -0400

    Danm you rovarsparketgit add .git add .!

--- Unit 2/src/RovarspraketTest.java
+++ Unit 2/src/RovarspraketTest.java
@@ -6,8 +6,11 @@ import org.junit.Test;
 public class RovarspraketTest {
 
   @Test
-  public void test() {
-    assertEquals();
+  public  void test() {
+    String loewrcase = "abaccaddefefeggehhijijikkillimmonnopopoqqorrossuttuvuvuwwuxxuyyuzzuz";
+    assertEquals(Rovarspraket.getOutput("a"),"a");
+    assertEquals(Rovarspraket.getOutput("b"),"bac");
+    assertEquals(Rovarspraket.getOutput("abcdefghijklmnopqrstuvwxyz"),loewrcase);
   }
 
 }
