c9cd751b6aeb50632d56e6866921351353368907
Pereira <073309882@educ.dpcdsb.org>
2017-03-27 09:35:58 -0400

    stuff

---
+++ Unit 2/src/TestFraction.java
@@ -0,0 +1,21 @@
+/**
+ * TestFraction
+ * 
+ * @author: Lance Pereira
+ * @course: ICS4U1
+ * @date: Mar 27, 2017
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class TestFraction {
+  public static void main(String args[]) {
+
+    Scanner sc = new Scanner(System.in);
+
+    Fraction f1 = new Fraction(3,4);
+    
+    System.out.println(f1);
+  }
+}
