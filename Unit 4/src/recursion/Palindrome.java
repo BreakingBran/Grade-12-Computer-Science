8ea134a75a494b5ac2ff7287cc643e6364198407
Pereira <073309882@educ.dpcdsb.org>
2017-05-29 09:01:57 -0400

    Fixed palindrome

--- Unit 4/src/recursion/Palindrome.java
+++ Unit 4/src/recursion/Palindrome.java
@@ -7,18 +7,21 @@ public class Palindrome {
   public static boolean isPalindrome(String word) {
 
     boolean isPalindrome = false;
+    word = word.toLowerCase();
+    word = word.replace(" ", "");
+    //System.out.println(word);
 
     if (word.length() <= 2) {
 
-      if (word.charAt(0) == word.charAt(1) || word.length() == 1) {
+      //Don't switch the order of the || expression as th other way can crash it
+      if (word.length() == 1 || word.charAt(0) == word.charAt(1)) {
         isPalindrome = true;
       }
       
     }else if (word.charAt(0) == word.charAt(word.length()-1)){
       isPalindrome = isPalindrome(word.substring(1, word.length()-1));
     }
-    
-    
+        
     return isPalindrome;
   }
   
