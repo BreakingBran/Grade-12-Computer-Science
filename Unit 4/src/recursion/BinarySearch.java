4a024e172ade4b893701d6dd560ade60ce1d2368
BreakingBran <lancepereira99@gmail.com>
2017-05-29 00:13:25 -0400

    Basic changes to palindrome

--- Unit 4/src/recursion/BinarySearch.java
+++ Unit 4/src/recursion/BinarySearch.java
@@ -4,38 +4,58 @@ import java.util.Arrays;
 
 import dataBase.Student;
 
-public class BinarySearch {
 
-  public static String binarySearch(String searchField, Student[] halvedStudentArray,
-      String criteria) throws Exception {
-    // FIXME Horribly broken, refcctor so that it can be used for anything or take out recursion
+/**
+ * Searches for the specified searchfield in a certain colunm of the database
+ * @param searchField: String of word you are looking for
+ * @param halvedStudentArray
+ * @param criteria
+ * @return
+ * @throws Exception
+ */
 
-    Student[] newHalvedArray;
-    int index = (int) halvedStudentArray.length / 2;
+public class BinarySearch{
+  
+public static String binarySearch(String searchField, Student[] halvedStudentArray, String criteria) throws Exception {
+  // FIXME Horribly broken, refcctor so that it can be used for anything or take out recursion
+  
+  Student[] newHalvedArray;
+  int index = (int) halvedStudentArray.length / 2;
+  String student;
 
-    // Check if equal
-    if (searchField.equals(halvedStudentArray[index].getstudentInfoDirectory(criteria))) {
-      return (halvedStudentArray[index].toString());
-    }
-
-    // Check if the search string is greater or less than middle element of halvedStudentArray
-    boolean isLess =
-        (searchField.compareToIgnoreCase(halvedStudentArray[index]
-            .getstudentInfoDirectory(criteria)) < 0);
-
-    if (isLess) {
-      newHalvedArray = Arrays.copyOfRange(halvedStudentArray, 0, index);
-    } else {
-      // the +1 is because the first index is inclusive
-      newHalvedArray = Arrays.copyOfRange(halvedStudentArray, index + 1, halvedStudentArray.length);
-    }
-
-    if (halvedStudentArray.length == 1
-        && !searchField.equals(halvedStudentArray[0].getstudentInfoDirectory(criteria))) {
-      return null;
-    } else {
-      return binarySearch(searchField, newHalvedArray, criteria);
-    }
-
+  // Check if equal
+  if (searchField.equals(halvedStudentArray[index].getstudentInfoDirectory(criteria))) {
+    return (halvedStudentArray[index].toString());
   }
+
+  // Check if the search string is greater or less than middle element of halvedStudentArray
+  boolean isLess = (searchField.compareToIgnoreCase(halvedStudentArray[index].getstudentInfoDirectory(criteria)) < 0);
+
+  if (isLess) {
+    newHalvedArray = Arrays.copyOfRange(halvedStudentArray, 0, index);
+  } else {
+    //the +1 is because the first index is inclusive 
+    newHalvedArray = Arrays.copyOfRange(halvedStudentArray, index+1, halvedStudentArray.length);
+  }
+
+  //I use contains because the courses functions are fucky 
+  if (halvedStudentArray.length == 2) {
+    if (searchField.contains(halvedStudentArray[0].getstudentInfoDirectory(criteria))){
+      student =  halvedStudentArray[0].toString();
+    }
+    else if (searchField.contains(halvedStudentArray[1].getstudentInfoDirectory(criteria))){
+      student =  halvedStudentArray[1].toString();
+    }
+    else{
+      student = null;
+    }
+    
+    return student;
+    
+  } else {
+    return binarySearch(searchField, newHalvedArray, criteria);
+  }
+
 }
+}
+
--- Unit 4/src/recursion/Palindrome.java
+++ Unit 4/src/recursion/Palindrome.java
@@ -1,5 +1,30 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class Palindrome {
+
+  public static boolean isPalindrome(String word) {
+
+    boolean isPalindrome = false;
+
+    if (word.length() <= 2) {
+
+      if (word.charAt(0) == word.charAt(1) || word.length() == 1) {
+        isPalindrome = true;
+      }
+      
+    }else if (word.charAt(0) == word.charAt(word.length()-1)){
+      isPalindrome = isPalindrome(word.substring(1, word.length()-1));
+    }
+    
+    
+    return isPalindrome;
+  }
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println(isPalindrome(sc.next()));
+  }
 
 }
