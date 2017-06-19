50858fe39d3d3852da8daecb5ddb6c0efbe9cc76
BreakingBran <lancepereira99@gmail.com>
2017-06-18 21:58:56 -0400

    Pain :(

--- CPT/src/lap/PatternMathcher.java
+++ CPT/src/lap/PatternMathcher.java
@@ -2,9 +2,10 @@ package lap;
 
 
 import java.awt.Color;
-import java.util.Arrays;
+import java.awt.Point;
 
 import robocode.AdvancedRobot;
+import robocode.GunTurnCompleteCondition;
 import robocode.HitWallEvent;
 import robocode.ScannedRobotEvent;
 import robocode.WinEvent;
@@ -16,32 +17,44 @@ public class PatternMathcher extends AdvancedRobot {
   int moveDirection = 20;
 
   StringBuilder pastEnemyMovements = new StringBuilder("");
-  
+  boolean turnStarted = false;
   long counter = 0;
 
+  double desiredGunRange = 0;
+  double X;
+  double Y;
+
+  double diffX;
+  double diffY;
+
+  double width;
+  double height;
+
+  int wallBarrier = 30;
 
   public void run() {
     // ...
     setBodyColor(Color.black);
     setGunColor(Color.black);
     setRadarColor(Color.orange);
-    turnRadarRightRadians(Double.POSITIVE_INFINITY);
 
-    /*
-     * char[] chars = new char[300]; Arrays.fill(chars, '0'); String result = new String(chars);
-     * 
-     * pastEnemyHeadings = new StringBuilder(result);
-     * 
-     * result = new String(Arrays.copyOfRange(chars, 0, 200));
-     * 
-     * pastEnemyVelocities = new StringBuilder(result);
-     * 
-     * System.out.println(pastEnemyHeadings.toString());
-     */
+    width = getBattleFieldWidth();
+    height = getBattleFieldHeight();
+
+    // sets the position variables
+    X = getX();
+    Y = getY();
 
     // Stops us from repositioning the radar for every change in
     // robot direction
     setAdjustRadarForRobotTurn(true);
+    setAdjustRadarForGunTurn(true);
+
+    turnRadarRightRadians(Double.POSITIVE_INFINITY);
+
+
+
+    System.out.println("Initial gun heading is: " + getGunHeading());
 
     do {
       // Check for new targets.
@@ -49,8 +62,10 @@ public class PatternMathcher extends AdvancedRobot {
       // pointed at the enemy and our onScannedRobot code doesn't end up telling
       // it to turn, so the system doesn't automatically call scan() for us
       // [see the javadocs for scan()].
-      setAhead(moveDirection);
+
+      // setAhead(moveDirection);
       // execute();
+
       scan();
 
     } while (true);
@@ -59,7 +74,7 @@ public class PatternMathcher extends AdvancedRobot {
   public void onScannedRobot(ScannedRobotEvent e) {
 
     counter += 1;
-    
+
     double radarTurn =
     // Absolute bearing to target
         getHeadingRadians() + e.getBearingRadians()
@@ -72,7 +87,8 @@ public class PatternMathcher extends AdvancedRobot {
     String formatedStringHeading = String.format("%03d", (int) e.getHeading());
     String formatedStringVelocity = formatVelocty(e.getVelocity());
 
-    //System.out.println("Heading: " + formatedStringHeading + " Velocity: " + formatedStringVelocity);
+    // System.out.println("Heading: " + formatedStringHeading + " Velocity: " +
+    // formatedStringVelocity);
 
     // pastEnemyHeadings.append("Hello");
 
@@ -80,18 +96,121 @@ public class PatternMathcher extends AdvancedRobot {
     pastEnemyMovements.append(formatedStringVelocity);
     pastEnemyMovements.append(";");
 
-    //Allows you to know that 100  turns have passed,
-    //Change to allow more or less data to be pattern matched
-    if (counter > 10) {
+
+    // Allows you to know that 100 turns have passed,
+    // Change to allow more or less data to be pattern matched
+    // controls how many past moves to pattern match against large move set
+    // controls how much data to store and pattern match
+    if (counter > 100) {
       pastEnemyMovements.delete(0, 6);
+
+      // Find a movement in the 0 to 95 elements of pastEnemyMovements that matches the last 5
+      // movements
+      // Use that to get a x2 and y2
+      // We want to get the last 5 elements, and the pastEnemyMovements is 100*6 characters long
+      String lastFiveEnemyMovements = pastEnemyMovements.substring(95 * 6, 100 * 6);
+
+      // finds index of first substring that matches this
+      int indexOfMatchedMovement = pastEnemyMovements.indexOf(lastFiveEnemyMovements);
+
+      // If a match is found that occured before the string that we gave in
+
+      // FIXME CHANGED FROM 95 TO 90
+      if (indexOfMatchedMovement <= 60 * 6 && !turnStarted && e.getDistance() < 400) {
+
+        System.out.println("A pattern has been matched");
+
+        // This will tryy to predict where the enemy will go next
+        double xMe = getX();
+        double yMe = getY();
+
+        // Gets the element after the matched movement
+        String nextMovement = pastEnemyMovements.substring(indexOfMatchedMovement + 39 * 6, indexOfMatchedMovement + 40 * 6);
+        int enemyHeading = Integer.parseInt(nextMovement.substring(0, 3));
+        int enemyVelocity = Integer.parseInt(nextMovement.substring(3, 5));
+
+        // This get the predicted difference in the enemy movements
+        // the sin and cos are switched for x and y as the angle is from North instead of + x axis
+        double xEnemyDiffirence = Math.sin(Math.toRadians(enemyHeading)) * enemyVelocity;
+        double yEnemyDiffirence = Math.cos(Math.toRadians(enemyHeading)) * enemyVelocity;
+
+        double xEnemyOriginal = xMe + Math.sin(getRadarHeadingRadians()) * e.getDistance();
+        double yEnemyOriginal = yMe + Math.cos(getRadarHeadingRadians()) * e.getDistance();
+
+        double xEnemyNew = xEnemyOriginal + xEnemyDiffirence;
+        double yEnemyNew = yEnemyOriginal + yEnemyDiffirence; // FIXME MADE THAT A NEGATIVE
+        // Are you feeling it now Mr.Krabs!!
+
+        double difX = xEnemyNew - xMe;
+        double difY = yEnemyNew - yMe;
+        double rotAng = -Math.toDegrees(Math.atan2(-difX, difY));
+
+        System.out.println("Rotang: " + rotAng);
+
+        // If rotAng is negative turn into + heading
+        if (rotAng < 0) {
+          rotAng = 360 + rotAng;
+        }
+
+        // Get the rotational now in terms of how much the gun has to move from the current position
+        rotAng = (rotAng - getGunHeading());
+
+        if (rotAng > 180) {
+          rotAng -= 360;
+        } else if (rotAng < -180) {
+          rotAng += 360;
+        }
+
+
+        /*
+         * System.out.println("Rotang after: " + rotAng);
+         * System.out.println("Detected at Radar Heading: " + getRadarHeading());
+         * System.out.println("Gun currently at (Degrees): " + getGunHeading());
+         * System.out.println("Gun rotation set at (Degrees): " + rotAng);
+         * System.out.printf("Enemy currently at: (%s, %s) \n", xEnemyOriginal,yEnemyOriginal);
+         * System.out.printf("Enemy predicted at: (%s, %s) \n", xEnemyNew,yEnemyNew);
+         */
+
+
+
+        // Try using Right
+        setTurnGunRight(rotAng);
+        turnStarted = true;
+      }
+
     }
-    System.out.println(pastEnemyMovements.toString());
+
+
+    // setAhead(moveDirection);
+
+
+    if (getGunTurnRemaining() <= 2 && turnStarted) {
+      setFire(1);
+      turnStarted = false;
+      System.out.println("Activated: " + getGunTurnRemaining());
+    } else {
+      System.out.println(getGunTurnRemaining());
+    }
+
+    // System.out.println(pastEnemyMovements.toString());
+    X = getX();
+    Y = getY();
+
+    // Find diffirence in self movement
+    diffX = Math.cos(getHeadingRadians()) * moveDirection;
+    diffY = Math.sin(getHeadingRadians()) * moveDirection;
+
+    if ((X + diffX) < wallBarrier || (X + diffX) > (width - wallBarrier) || (Y + diffY) < (wallBarrier) || (Y + diffY) > (height - wallBarrier)) {
+      System.out.println(width + " " + height);
+      moveDirection = -moveDirection;
+    }
+    setAhead(moveDirection);
     setTurnRadarRightRadians(1 * Utils.normalRelativeAngle(radarTurn));
+
 
 
     // ...
   }
-
 
   private String formatVelocty(double velocity) {
     String formatedVelocity = Integer.toString((int) velocity);
@@ -107,6 +226,9 @@ public class PatternMathcher extends AdvancedRobot {
   }
 
   public void onHitWall(HitWallEvent e) {
-    moveDirection = -moveDirection;// reverse direction upon hitting a wall
+    // moveDirection = -moveDirection;// reverse direction upon hitting a wall
+
+    setBack(wallBarrier);
+
   }
 }
---
+++ CPT/src/testingFunctions/testingFunction1.java
@@ -0,0 +1,14 @@
+package testingFunctions;
+
+public class testingFunction1 {
+
+  public static void main(String[] args) {
+    double x = 0;
+    double y = 0;
+    double x2 = -5;
+    double y2 = 3;
+    System.out.println(Math.toDegrees(Math.atan2(x2 - x, y2 - y)));
+    
+  }
+
+}
